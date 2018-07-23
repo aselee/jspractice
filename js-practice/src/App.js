a1ae8787c495ad5745ca6823eda4212e49c9bc24
text-edit <DanielMcKee@Daniels-MacBook-Pro-15.local>
2018-07-23 14:59:27 -0500

    text-edit changes

--- js-practice/src/App.js
+++ js-practice/src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React Daniel you little punk</h1>
         </header>
+        jdfklajdfklasjdflasdkjf
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
