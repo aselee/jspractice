b51065cc035ba70b5f06273cb98578007399b11e
text-edit <DanielMcKee@Daniels-MacBook-Pro-15.local>
2018-07-23 16:05:43 -0500

    LAST TEST FOR TODAY

--- js-practice/src/App.js
+++ js-practice/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React Daniel you little punk</h1>
         </header>
-        NEW TEST AFTER LOCAL COFFEE 3:48 pm 
+        LAST TEST BEFORE ANDY LEAVES 4:04PM
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
