0525fd1513adaca783a8ec22cf403722d27ea7ac
Andrew <32229265+aselee@users.noreply.github.com>
2018-08-12 16:42:27 -0500

    JS operator notes

---
+++ js-practice/src/Operators.js
@@ -0,0 +1,21 @@
+// Operators in JS
+
+/*
+Assignment: = as in a = 2.
+
+Math: + (addition), - (subtraction), * (multiplication), / (division).
+
+Compound Assignment: +=, -=, *=, and /= are compund operators that combine a math operation with assignment, as in a += 2 (same as a = a + 2).
+
+Increment/ Decrement: ++ (increment), -- (decrement), as in a++ (similar to a = a + 1).
+
+Object property Access: . as in console.log().
+Objects are values that hold other values at specific named locations called properties. obj.a means an object value called obj with a property of the name a. 
+Properties can alternatively be accessed as obj["a"]. 
+
+Equality: == (loose-equals), ===(strict-equals), != (loose not-equals), !== (strict not-equals), as in a == b.
+
+Comparison: < (less than), > (greater than), <= (less than or loose-equals), >= (greater than or loose-equals), as in a <= b.
+
+Logical: && (and), || (or), as in a || b that selects either a or b.
+*/
